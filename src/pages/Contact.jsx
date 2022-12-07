404ed3f034d49d1b116d8d83333d1da28ae05cff
Nitieii <tuananhngo2513@gmail.com>
2022-12-07 16:20:41 +0700

    nothing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,5 @@
 const Contact = () => {
-  return (
-    <main id="main" data-aos="fade-up">
-      <section class="breadcrumbs">
-        <div class="container">
-          <div class="d-flex justify-content-between align-items-center">
-            <h2>Inner Page</h2>
-            <ol>
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>Inner Page</li>
-            </ol>
-          </div>
-        </div>
-      </section>
-
-      <section class="inner-page">
-        <div class="container">
-          <p>Example inner page template</p>
-        </div>
-      </section>
-    </main>
-  );
+  return <main id="main" data-aos="fade-up"></main>;
 };
 
 export default Contact;
